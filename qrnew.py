8a392dd3cab1c1a07e84e16387643dbd187e0eec
AbdulHadi-Programmer <122735121+AbdulHadi-Programmer@users.noreply.github.com>
2023-06-04 05:00:41 -0700

    Add files via upload

---
+++ qrnew.py
@@ -0,0 +1,11 @@
+import qrcode
+from PIL import Image
+qr = qrcode.QRCode(version=1, box_size=10, error_correction = qrcode.constants.ERROR_CORRECT_H, border= 4,)
+qr.add_data("https://github.com/AbdulHadi-Programmer/")
+# qr.add_data(input("Enter the data to be Encoded: "))
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color= 'White')
+img.save("git.png")
+
+
+
