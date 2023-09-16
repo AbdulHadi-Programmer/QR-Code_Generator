89a2afbe9370c9fde82b6452a7fe0d49e78885d3
Abdul Hadi <122735121+AbdulHadi-Programmer@users.noreply.github.com>
2023-09-16 16:41:38 +0500

    Update qrnew.py

--- qrnew.py
+++ qrnew.py
@@ -4,4 +4,4 @@ qr = qrcode.QRCode(version=1, box_size=10, error_correction = qrcode.constants.E
 qr.add_data(input("Enter the data to be Encoded: "))
 qr.make(fit=True)
 img = qr.make_image(fill_color="black", back_color= 'White')
-img.save("git.png")
+img.save("code.png")
